259da22efb92d90e99e4fa5abd14ae7a72257759
Kennedy Rodrigo Torres Gonçalves <128331199+Kennedy-Torres@users.noreply.github.com>
2024-06-17 18:47:25 -0300

    fix: fechamento do scanner

--- src/main/java/socket/Cliente.java
+++ src/main/java/socket/Cliente.java
@@ -20,47 +20,41 @@ public class Cliente {
 			DataOutputStream output = new DataOutputStream(cliente.getOutputStream());
 			final DataInputStream input = new DataInputStream(cliente.getInputStream());
 
-
 			/**
-			 * Utilizamos Thread para garantir o paralelismo entre cliente e servidor:
-			 * 1. Ler mensagens enviadas do servidor
-			 * 2. Ao mesmo tempo enviar mensagem ao servidor
+			 * Utilizamos Thread para garantir o paralelismo entre cliente e servidor: 1.
+			 * Ler mensagens enviadas do servidor 2. Ao mesmo tempo enviar mensagem ao
+			 * servidor
 			 */
 			/* 3. RECEBENDO MENSAGEM DO SERVIDOR */
-			final Socket finalCliente = cliente; //a referência ao objeto Socket não mudará
+			final Socket finalCliente = cliente; // a referência ao objeto Socket não mudará
 			new Thread(() -> {
 				try {
 					while (!finalCliente.isClosed()) {
-						try{
+						try {
 							String mensagemDoServidor = input.readUTF();
 							System.out.println(mensagemDoServidor);
-						}catch(EOFException e){
-							// conexao do servidor foi encerrada enquanto o cliente tentava enviar uma mensagem
-							System.out.println("O cliente perdeu a conexão com o servidor." +
-									"\nPara encerrar digite '::sair'");
+						} catch (EOFException e) {
+							// conexao do servidor foi encerrada enquanto o cliente tentava enviar uma
+							// mensagem
+							System.out.println(
+									"O cliente perdeu a conexão com o servidor." + "\nPara encerrar digite '::sair'");
 							break;
-						}catch (SocketException e){ // ---> lida com desconexões inesperadas do servidor
+						} catch (SocketException e) { // ---> lida com desconexões inesperadas do servidor
 							System.out.println("Conexão do servidor foi encerrada de forma inesperada.");
 							break;
 						}
 					}
 				} catch (IOException e) {
 					e.printStackTrace();
-				}finally {
-					try {
-						finalCliente.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
 				}
 			}).start();
 
 			/* 4. ESCREVENDO MENSAGEM PARA O SERVIDOR */
 			Scanner consoleDoCliente = new Scanner(System.in);
-			try{
+			try {
 				while (true) {
 					String mensagemDoCliente = consoleDoCliente.nextLine();
-					try{
+					try {
 						if (mensagemDoCliente.equalsIgnoreCase("::sair")) {
 							System.out.println("Encerrando o cliente...");
 							break;
@@ -68,25 +62,36 @@ public class Cliente {
 						output.writeUTF(mensagemDoCliente);
 						output.flush();
 
-					}catch (EOFException e) {
-						// conexao do servidor foi encerrada enquanto o cliente tentava enviar uma mensagem
+					} catch (EOFException e) {
+						// conexao do servidor foi encerrada enquanto o cliente tentava enviar uma
+						// mensagem
 						e.printStackTrace();
 						break;
-					}catch (SocketException e) { // ---> lida com desconexões inesperadas do cliente
+					} catch (SocketException e) { // ---> lida com desconexões inesperadas do cliente
 						System.out.println("O cliente não consegue mais enviar mensagem.");
 						break;
 					}
 				}
-			}catch (IOException e){
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 
+			consoleDoCliente.close();
+			output.close();
+			input.close();
 		} catch (UnknownHostException e) {
 			System.out.println("Problema no host");
 			System.err.println("Erro: " + e.getMessage());
 		} catch (IOException e) {
 			System.out.println("Problema na porta (Porta está oculpada ou fora do ar)");
 			System.err.println("Erro: " + e.getMessage());
+		} finally {
+			try {
+				cliente.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 }
